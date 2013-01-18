987229882746307467968f68fbeac3d0e782dca1
imranansari <imran.iansari@gmail.com>
2013-01-18 16:30:06 -0500

    lfg updates

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -4,7 +4,10 @@ class PdfController < ApplicationController
 
     @respArr = Array.new
 
-    @respArr.push({:name => "60 Reasons", :url => "http://localhost:3000/60ReasonsWireFrame.pdf"})
+    @respArr.push({:name => "American Legacy", :url => "http://localhost:3000/AL-APTDS-BRC280_Z02_VIEW.pdf"})
+    @respArr.push({:name => "CP Income Solutions", :url => "http://localhost:3000/CP-APTSN-BRC281_Z03_VIEW.pdf"})
+    @respArr.push({:name => "CP Solutions", :url => "http://localhost:3000/CP-APTSP-BRC001_Z03_VIEW.pdf"})
+    @respArr.push({:name => "CP Client Guide", :url => "http://localhost:3000/CP-INC-BRC001_Z03_VIEW.pdf"})
 
     respond_to do |format|
       format.json { render json: @respArr }
